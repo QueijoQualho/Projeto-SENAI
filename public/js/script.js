2fa94a470d96f7880cddbd5f20f9f36bedf65c8b
Pedro Jesus <134734704+QueijoQualho@users.noreply.github.com>
2023-10-02 23:09:24 -0300

    update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,18 @@
 const botao = document.getElementById("enviar")
-
-
 botao.addEventListener('click', () => {
     const nomeJogo = document.getElementById("nomeJogo").value;
-    fetch(`\\api\\${nomeJogo}`)
-        .then(response => response.json())
+    fetch(`/api/${nomeJogo}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
         .then((data) => {
+            /* Vai ter que fazer o card aqui */
             console.log(data);
         })
-        .catch()
+        .catch((error) => {
+            console.error('Fetch error:', error);
+        });
 })

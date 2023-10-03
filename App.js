bbfb6045763d0dfc28e8543200856a4873e35bc1
QueijoQualho <pedromra.04@gmail.com>
2023-10-02 21:37:15 -0300

    Update dependencies and remove body-parser.

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser')
 const router = require('./router/api.js')
 require("dotenv").config();
 
 const app = express();
 const porta = process.env.PORT;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.use(express.json());
 
 app.use('/', router)

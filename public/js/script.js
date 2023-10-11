e8827c70d8e1b0d4176bb53835218cf1756ecf1e
EverthonLeite <leverthon9120@gmail.com>
2023-10-11 19:05:27 -0300

    Added search bar and sort functionality

--- public/js/script.js
+++ public/js/script.js
@@ -16,3 +16,47 @@ botao.addEventListener('click', async () => {
         console.error("Ocorreu um erro:", error);
     }
 });
+
+/* document.addEventListener('DOMContentLoaded', function () {
+    const sortSelect = document.getElementById('sort-select');
+    const productList = document.getElementById('product-list');
+
+    // Dados de exemplo (substitua por seus próprios dados)
+    const products = [
+        { nome: 'Produto A', preco: 10.99, categoria: 'Eletrônicos' },
+        { nome: 'Produto B', preco: 5.99, categoria: 'Roupas' },
+        { nome: 'Produto C', preco: 25.99, categoria: 'Eletrônicos' },
+        // Adicione mais produtos aqui
+    ];
+
+    // FuAddnção para renderizar a lista de produtos
+    function renderProducts(products) {
+        productList.innerHTML = '';
+        products.forEach(product => {
+            const li = document.createElement('li');
+            li.textContent = `${product.nome} - Preço: R$${product.preco} - Categoria: ${product.categoria}`;
+            productList.appendChild(li);
+        });
+    }
+
+    // Evento de mudança de seleção
+    sortSelect.addEventListener('change', function () {
+        const sortBy = sortSelect.value;
+
+        // Classificar os produtos com base na opção selecionada
+        if (sortBy === 'nome') {
+            products.sort((a, b) => a.nome.localeCompare(b.nome));
+        } else if (sortBy === 'preco') {
+            products.sort((a, b) => a.preco - b.preco);
+        } else if (sortBy === 'categoria') {
+            products.sort((a, b) => a.categoria.localeCompare(b.categoria));
+        }
+
+        // Renderizar a lista de produtos classificados
+        renderProducts(products);
+    });
+
+    // Inicialmente, renderize a lista de produtos com base na opção padrão
+    renderProducts(products);
+});
+ */

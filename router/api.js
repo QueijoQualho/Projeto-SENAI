e025b6c93600dd7fe1f2cb6caeba9885d9fc0cb3
QueijoQualho <pedromra.04@gmail.com>
2023-10-11 20:57:57 -0300

    Update api

--- router/api.js
+++ router/api.js
@@ -4,10 +4,23 @@ const ApiSteam = require('../controller/apiController.js');
 const router = express.Router();
 const apiSteam = new ApiSteam();
 
+router.get('/api/test/:value', async (req, res) => {
+  const value = req.params.value;
+
+  try {
+    const gameNames = await apiSteam.getGameNames(value);
+
+    res.json(gameNames);
+  } catch (error) {
+    console.error('Erro ao buscar nomes de jogos:', error);
+    res.status(500).json({ error: 'Um erro interno ocorreu ao buscar nomes de jogos' });
+  }
+});
+
 router.get('/api/:index', async (req, res) => {
   const nomeJogo = req.params.index;
   try {
-    const idJogo = await apiSteam.getNameGame(nomeJogo);
+    const idJogo = await apiSteam.getIdGame(nomeJogo);
 
     if (idJogo == null) {
       res.status(404).json({ error: 'Jogo não encontrado' });
@@ -24,6 +37,7 @@ router.get('/api/:index', async (req, res) => {
     res.status(500).send('Um erro foi encontrado');
   }
 });
+
 
 /* Usar isso na hora de ordenar a lista ordem de arquivo */
 /* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort */

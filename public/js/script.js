fa049e8be649bd59cfd66f2d05ea76856daa0e75
QueijoQualho <pedromra.04@gmail.com>
2023-10-11 21:08:47 -0300

    update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,35 +1,35 @@
-    const botao = document.getElementById("enviar")
-    const input = document.getElementById("nomeJogo")
+const botao = document.getElementById("enviar")
+const input = document.getElementById("nomeJogo")
 
-    botao.addEventListener('click', async () => {
-        try {
-            const nomeJogo = document.getElementById("nomeJogo").value;
-            
-            const url = `/api/${nomeJogo}`
-            const response = await fetch(url);
+botao.addEventListener('click', async () => {
+    try {
+        const nomeJogo = document.getElementById("nomeJogo").value;
 
-            if (!response.ok) {
-                console.log("Jogo não encontrado");
-                return;
-            }
+        const url = `/api/${nomeJogo}`
+        const response = await fetch(url);
 
-            const data = await response.json();
-            console.log(data);
-        } catch (error) {
-            console.error("Ocorreu um erro:", error);
+        if (!response.ok) {
+            console.log("Jogo não encontrado");
+            return;
         }
-    });
 
-    input.addEventListener('input', async (e)=>{
-        try {
-            const value = e.target.value;
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error("Ocorreu um erro:", error);
+    }
+});
 
-            const url = `/api/test/${value}`
-            const response = await fetch(url)
-            const data = await response.json();
-            console.log(data);
-        } catch (error) {
-            
-        }
-    })
+input.addEventListener('input', async (e) => {
+    try {
+        const value = e.target.value;
+
+        const url = `/api/test/${value}`
+        const response = await fetch(url)
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error("Ocorreu um erro:", error);
+    }
+})
 

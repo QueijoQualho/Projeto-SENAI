4046b15513b626c7df5b414337f1ff99d9985d84
RenanRochella <reveg48@gmail.com>
2023-10-30 21:29:00 -0300

    tá indo

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,10 @@
 const input = document.getElementById("caixaPesquisa")
+const btn = document.getElementById("icone-lupa")
 const datalist = document.getElementById('options');
 
-const banner = document.getElementById("fazoL")
+const banner = document.getElementById("card")
 
 
-
-/* btn.addEventListener('click', async (e) => {
-    try {
-        
-    } catch (error) {
-        
-    }
-})
- */
 /* Tem que fazer o botão pra enviar ou algo parecido */
 
 /* botao.addEventListener('click', async () => {
@@ -56,5 +48,54 @@ input.addEventListener('input', async (e) => {
 });
 
 
+btn.addEventListener('click', async () => {
 
+    const nomeJogo = input.value;
+
+    const url = `/api/${nomeJogo}`
+    const response = await fetch(url);
+
+    if (!response.ok) {
+        console.log("Jogo não encontrado");
+        return;
+    }
+
+    const data = await response.json();
+
+    data.achievements.forEach(element => {
+        let belela = document.createElement('div')
+        belela.classlist.add('nome-game')
+
+        let zarabatana = document.createElement('div')
+        zarabatana.classList.add('conquistas')
+
+        babela.innerHTML = `
+        
+        <div class="foto-jogo">
+            <img src="${data.image}" alt="">
+        </div>
+        <div class="titulo-jogo">
+            <h1>${input.value}</h1>
+        </div>
+    `
+        zarabatana.innerHTML = `
+        <div class="foto-conquista">
+            <img src="${element.image}" alt="">
+        </div>
+        <div class="nome-conquista">
+            <h1>${element.displayName}</h1>
+        </div>
+        <div class="porcentagem">
+            <h1>${element.percent}</h1>
+        </div>  
+    `
+    banner.appendChild(babela);
+    banner.appendChild(zarabatana);
+
+    })
+
+
+
+
+})
 
